177e9810df84df8841740cd5b8573ab760602040
joaorleaoo <jrlgo@ic.ufal.br>
2024-05-25 21:09:04 -0300

    feat: Desafio: Smart Tv usando métodos em Java

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,30 @@
-public class main {
+package src;
+
+public class Main {
+    public static void main(String []args){
+        SmartTv minhaTv = new SmartTv();
+
+        System.out.println("A minha tv esta ligada ? " + minhaTv.estaLigada);
+        System.out.println("A minha tv esta no volume " + minhaTv.volume);
+        System.out.println(minhaTv.canal);
+
+        minhaTv.canal = 50;
+
+        System.out.println(minhaTv.canal);
+        minhaTv.ligarTv();
+        System.out.println("A minha tv esta ligada ? " + minhaTv.estaLigada);
+        minhaTv.desligarTv();
+
+        minhaTv.aumentarVolume();
+        minhaTv.aumentarVolume();
+        minhaTv.aumentarVolume();
+        minhaTv.diminuirVolume();
+
+        minhaTv.diminuirCanal();
+        minhaTv.diminuirCanal();
+        minhaTv.aumentarCanal();
+        minhaTv.mudaDeCanal(80);
+
+
+    }
 }
---
+++ src/SmartTv.java
@@ -0,0 +1,31 @@
+package src;
+
+public class SmartTv {
+    boolean estaLigada = false;
+    byte volume = 2;
+    int canal =30;
+
+
+    public void ligarTv(){
+        estaLigada = true;
+    }
+    public void desligarTv(){
+        estaLigada = false;
+    }
+    public void aumentarVolume(){
+        System.out.println("Aumentando o volume para: "+ ++volume);
+    }
+    public void diminuirVolume(){
+        System.out.println("Diminuindo o volume para: "+ --volume);
+    }
+    public void mudaDeCanal(int novoCanal){
+        canal = novoCanal;
+        System.out.println("Mudando o canal para: "+ canal);
+    }
+    public void aumentarCanal(){
+        System.out.println("Mudando o canal para: "+ ++canal);
+    }
+    public void diminuirCanal(){
+        System.out.println("Mudando o canal para: "+ --canal);
+    }
+}
